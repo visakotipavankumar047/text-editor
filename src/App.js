b84f7587608d53fa7f854ac82ea95b0d939fbbba
visakotipavankumar047 <pavankumarvisakoti047@gmail.com>
2024-01-24 01:13:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import RichEditor from './components/RichTextEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> RichTextEditor </h1>
+      <RichEditor />
+    </div>
+  );
+}
+
+export default App;
